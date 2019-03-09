7f3b908059e92dbe13beef2810ff5d954e2ea668
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-03-09 12:11:58 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM continuumio/miniconda3:4.5.11
+
+RUN apt-get update -y; apt-get upgrade -y
+RUN apt-get update -y; apt-get upgrade -y; apt-get install -y vim-tiny vim-athena ssh
+
+RUN adduser --home /home/airflow airflow
+
+RUN mkdir -p /home/airflow/airflow
+RUN mkdir -p /home/airflow/.ssh
+COPY airflow/airflow.cfg /home/airflow/airflow/airflow.cfg
+COPY airflow/airflow_cors.patch /home/airflow
+RUN chown -R airflow:airflow /home/airflow/airflow
+RUN chown -R airflow:airflow /home/airflow/.ssh
+
+USER airflow
+WORKDIR /home/airflow
+
+COPY environment.yml environment.yml
+
+RUN conda env create -f environment.yml
+RUN echo "alias l='ls -lah'" >> ~/.bashrc
+RUN echo "source activate airflow" >> ~/.bashrc
+
+ENV CONDA_EXE /opt/conda/bin/conda
+ENV CONDA_PREFIX /home/airflow/.conda/envs/airflow
+ENV CONDA_PYTHON_EXE /opt/conda/bin/python
+ENV CONDA_PROMPT_MODIFIER (airflow)
+ENV CONDA_DEFAULT_ENV airflow
+ENV PATH /home/airflow/.conda/envs/airflow/bin:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+RUN mv airflow_cors.patch /home/airflow/.conda/envs/airflow/lib && \
+    cd /home/airflow/.conda/envs/airflow/lib && \
+    patch python3.6/site-packages/airflow/www/app.py airflow_cors.patch
+
+# For this tutorial I don't need root access, but if you need to mount a directory with read/write access, just run everything as root
+# If you do this tho celery complains, loudly
+#USER root
+#
+#RUN mkdir $HOME/airflow
+#RUN cp /home/airflow/airflow/airflow.cfg $HOME/airflow/airflow.cfg
+#
+#ENV C_FORCE_ROOT true
+#ENV CONDA_EXE /opt/conda/bin/conda
+#ENV CONDA_PREFIX /home/airflow/.conda/envs/airflow
+#ENV CONDA_PYTHON_EXE /opt/conda/bin/python
+#ENV CONDA_PROMPT_MODIFIER (airflow)
+#ENV CONDA_DEFAULT_ENV airflow
+#ENV PATH /home/airflow/.conda/envs/airflow/bin:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
---
+++ dags/icecream_sunday_dag_def.py
@@ -0,0 +1,82 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
+import random
+from pprint import pprint
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+dag = DAG('ice_cream_sundae', default_args=default_args, schedule_interval=None)
+
+sundae_choices = {
+    'cones': ['cup', 'waffle'],
+    'toppings': ['rainbow sprinkles', 'chocolate sprinkles', 'm&ms', 'gummy bears', 'chocolate sauce',
+                 'strawberry sauce', 'caramel sauce', 'marshmallow'],
+    'ice_cream_flavors': ['vanilla', 'chocolate', 'strawberry', 'salted caramel', 'cotton candy'],
+}
+
+
+def choose_cone(ds, **kwargs):
+    kwargs['ti'].xcom_push(key='topping', value=random.choice(sundae_choices['cones']))
+
+
+choose_cone_op = PythonOperator(
+    task_id='choose_cone_task',
+    python_callable=choose_cone,
+    provide_context=True,
+    dag=dag
+)
+
+
+def choose_icecream_flavor(ds, **kwargs):
+    kwargs['ti'].xcom_push(key='ice_cream_flavor', value=random.choice(sundae_choices['ice_cream_flavors']))
+
+
+choose_icecream_flavor_op = PythonOperator(
+    task_id='choose_icecream_flavor_task',
+    provide_context=True,
+    dag=dag,
+    python_callable=choose_icecream_flavor,
+)
+
+
+def choose_toppings(ds, **kwargs):
+    kwargs['ti'].xcom_push(key='toppings', value=random.choice(sundae_choices['toppings']))
+
+
+choose_toppings_op = PythonOperator(
+    task_id='choose_toppings_task',
+    provide_context=True,
+    dag=dag,
+    python_callable=choose_toppings,
+)
+
+
+def make_icecream_sundae(ds, **kwargs):
+    # Get the task instance
+    ti = kwargs['ti']
+    cone = ti.xcom_pull(key='cone', task_ids='choose_cone_task')
+    icecream_flavor = ti.xcom_pull(key='ice_cream_flavor', task_ids='choose_icecream_flavor_task')
+    topping = ti.xcom_pull(key='choose_toppings_task', task_ids='choose_toppings_task')
+    print('Our sundae is complete!')
+    pprint({'cone': cone, 'icecream_flavor': icecream_flavor, 'topping': topping})
+
+
+make_icecream_sundae_op = PythonOperator(
+    task_id='make_icecream_sundae_task',
+    provide_context=True,
+    dag=dag,
+    python_callable=make_icecream_sundae,
+)
+
+choose_cone_op.set_upstream(choose_icecream_flavor_op)
+choose_icecream_flavor_op.set_upstream(choose_toppings_op)
+make_icecream_sundae_op.set_upstream(choose_icecream_flavor_op)
